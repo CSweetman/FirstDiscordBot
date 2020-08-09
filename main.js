8252857fc97de1d8268b09106b9c4b7d789f93c2
Christopher Sweetman <chrisjdsweetman@gmail.com>
2020-08-09 16:56:23 -0500

    Changes: Foundation of discord bot set up/code. Gave bot all permissions and it can detect the prefix of the command and splice the command to be able to detect arguments.

---
+++ main.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');  //Lets us communicate with Discord tools
+
+const client = new Discord.Client();    //Instance of our bot object interacting that contains discord tools
+
+const prefix = '-';     //prefix of commands it is listening for
+
+
+client.once('ready', ()=> {
+    console.log('Starter is online!');
+});
+
+client.on('message', message =>{        //Look up arrow function
+    if(!message.content.startsWith(prefix) || message.author.bot) return;       
+
+    const args = message.content.slice(prefix.length).split(/ +/);      //Look up syntax of this
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('pong');
+    }
+    else if(command === 'youtube'){
+        message.channel.send('https://www.youtube.com');
+    }
+});
+
+
+
+
+
+client.login('NzQyMTI4NTU3MDY0NTg1MzA3.XzBnFA.16lMC3nFYC8vT4SuoSHQI7rN5H4');
