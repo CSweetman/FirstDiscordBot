ddfdaa571729a3866cf7649c8c9a5da913fa6838
Christopher Sweetman <chrisjdsweetman@gmail.com>
2020-08-09 19:54:57 -0500

    Changes: Revised bot to access commands via commands folder that contains all the bot commands to handle commands in a more organized fashion rather than having one big main.js file.
    Security Concerns: N/A

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const Discord = require('discord.js');  //Lets us communicate with Discord tools
-
 const client = new Discord.Client();    //Instance of our bot object interacting that contains discord tools
 
 const prefix = '-';     //prefix of commands it is listening for
+
+const fs = require('fs');
+
+client.commands = new Discord.Collection(); //Creates a discord collection
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));    //looks into command files and ensures we are reading a javascript file.
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 
 
 client.once('ready', ()=> {
@@ -16,10 +25,7 @@ client.on('message', message =>{        //Look up arrow function
     const command = args.shift().toLowerCase();
 
     if(command === 'ping'){
-        message.channel.send('pong');
-    }
-    else if(command === 'youtube'){
-        message.channel.send('https://www.youtube.com');
+        client.commands.get('ping').execute(message, args);
     }
 });
 
@@ -27,4 +33,4 @@ client.on('message', message =>{        //Look up arrow function
 
 
 
-client.login('NzQyMTI4NTU3MDY0NTg1MzA3.XzBnFA.16lMC3nFYC8vT4SuoSHQI7rN5H4');
+client.login('NzQyMTI4NTU3MDY0NTg1MzA3.XzBnFA.MnkgbA-ikxkDVMmGREmnwWGs204');
